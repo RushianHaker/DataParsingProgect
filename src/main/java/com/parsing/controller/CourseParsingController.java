31b9760ed809070c29791f422ba52026b45c0cf1
Max Ivanov <mamalenaralalend@gmail.com>
2022-10-25 15:53:50 +0300

    test

--- src/main/java/com/parsing/controller/CourseParsingController.java
+++ src/main/java/com/parsing/controller/CourseParsingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * CourseParsingController - реализация интерфейса базовых методов CourseEntityRepository
+ * CourseParsingController
  *
  * @author max
  */
